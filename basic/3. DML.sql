809f7f9825806c2e12e280d408367afe9d2da2bc
GreatJang <1wkdwnsgur@gmail.com>
2023-11-17 11:18:35 +0900

    Commit

--- basic/3. DML.sql
+++ basic/3. DML.sql
@@ -10,3 +10,19 @@ select name from author;
 select * from author where id = 1;
 select * from author where id > 10;
 select * from author where id > 10 and name = 'abc';
+
+-- 이름 중복제거하고 조회
+select distinct name from author;
+
+-- order by
+select * from author order by name desc;
+
+-- order by 멀티 : 먼저 쓴 컬럼 우선정렬. asc/desc 생략시 asc 적용
+select * from author order by name, email desc;
+
+-- limit number : 특정숫자로 결과값 개수 제한
+select * from author order by id desc limit 2;
+
+-- alias를 이용한 select문(as는 생략가능)
+select name as 이름 from author;
+select name, email from author as a;
